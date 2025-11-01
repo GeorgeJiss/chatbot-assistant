6d26c2335fa996854f4c31a9b9215e0ba8d34e2f
GeorgeJiss <jerinjiss001@gmail.com>
2025-11-01 11:48:34 +0530

    fixed errors caused by voice-service

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,7 +1,6 @@
 from src.services.llm_service import LLMService
 from src.services.validation_service import ValidationService
 from src.services.storage_service import StorageService
-from src.services.voice_service import VoiceService
 from src.services.sentiment_service import SentimentService
 from src.services.language_service import LanguageService
 
@@ -9,7 +8,6 @@ __all__ = [
     'LLMService',
     'ValidationService',
     'StorageService',
-    'VoiceService',
     'SentimentService',
     'LanguageService'
 ]
